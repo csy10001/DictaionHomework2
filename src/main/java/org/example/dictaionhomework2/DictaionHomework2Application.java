d2b22f35c6a296215b705e7d7271722001cc0729
csy10001 <fuitshine@gmail.com>
2025-07-31 09:54:52 +0900

    first commit

---
+++ src/main/java/org/example/dictaionhomework2/DictaionHomework2Application.java
@@ -0,0 +1,13 @@
+package org.example.dictaionhomework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictaionHomework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictaionHomework2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/dictaionhomework2/DictaionHomework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dictaionhomework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictaionHomework2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
